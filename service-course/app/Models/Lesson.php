7e4f17d1ccceba42c0be369fe1e5a9d482100cbc
Muhammad Rifki <69866073+rifki-sgnic@users.noreply.github.com>
2023-02-01 03:31:09 +0700

    Develop Service Course

---
+++ service-course/app/Models/Lesson.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lesson extends Model
+{
+    use HasFactory;
+
+    protected $table = 'lessons';
+    protected $fillable = [
+        'name', 'chapter_id', 'video'
+    ];
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:m:s',
+        'updated_at' => 'datetime:Y-m-d H:m:s'
+    ];
+}
